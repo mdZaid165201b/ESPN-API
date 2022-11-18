990088738fccbd59ad7d045b239dcd8fcdc5ecdf
Muhammad Zaid <107930247+mdZaid165201b@users.noreply.github.com>
2022-11-19 01:38:36 +0500

    Update ranking.js

--- Routes/ranking.js
+++ Routes/ranking.js
@@ -1,89 +1,132 @@
-const request = require("request-promise");
-const cheerio = require("cheerio");
-
-let ranking = {
-  test: [],
-  odi: [],
-  t20: [],
-};
-const baseTeamRankingURL =
-  "https://www.espncricinfo.com/rankings/content/page/211271.html";
-
-
-
-const getTeamsRanking = async (link = baseTeamRankingURL) => {
-  const { $ } = await getHtml(link);
-  const rankingObj = await getRankingObj($);
-  let testRanking = rankingObj.TEST.children;
-  let odiRanking = rankingObj.ODI.children;
-  let t20Ranking = rankingObj.T20I.children;
-
-  await makeList(testRanking, ranking.test, $);
-  await makeList(odiRanking, ranking.odi, $);
-  await makeList(t20Ranking, ranking.t20, $);
-  return ranking;
-};
-
-const getTestTeamRanking = async (link = baseTeamRankingURL) => {
-  const { $ } = await getHtml(link);
-  const rankingObj = await getRankingObj($);
-  let testRanking = rankingObj.TEST.children;
-  await makeList(testRanking, ranking.test, $);
-  return ranking.test;
-};
-
-const getODITeamRanking = async (link = baseTeamRankingURL) => {
-  const { $ } = await getHtml(link);
-  const rankingObj = await getRankingObj($);
-  let odiRanking = rankingObj.ODI.children;
-  await makeList(odiRanking, ranking.odi, $);
-  return ranking.odi;
-};
-
-const getT20TeamRanking = async (link = baseTeamRankingURL) => {
-  const { $ } = await getHtml(link);
-  const rankingObj = await getRankingObj($);
-  let t20Ranking = rankingObj.T20I.children;
-  await makeList(t20Ranking, ranking.t20, $);
-  return ranking.t20;
-};
-
-/*-------------getTeamsRanking Utility Methods-------------*/
-const getRankingObj = async ($) => {
-  const ranking_html = await $(".StoryengineTable tbody:lt(3)");
-  const rankingName = ["TEST", "ODI", "T20I"];
-  let rankingHTMLOBJ = {};
-  for (let index = 0; index < ranking_html.length; index++) {
-    rankingHTMLOBJ[rankingName[index]] = ranking_html[index];
-  }
-  return rankingHTMLOBJ;
-};
-
-const getHtml = async (link) => {
-  let html = await request.get(link);
-  let $ = await cheerio.load(html);
-  return { $, html };
-};
-
-const makeList = (type, rankingArr, $) => {
-  for (let i = 2; i < type.length; i += 2) {
-    let team = type[i].children;
-
-    rankingArr.push({
-      pos: $(team[1]).text(),
-      team: $(team[3]).text(),
-      matched: $(team[5]).text(),
-      points: $(team[7]).text(),
-      rating: $(team[9]).text(),
-    });
-  }
-};
-/*---------------end of getTeamsRaning utility Methods---------------*/
+const router = require("express").Router();
+const {
+    getODITeamRanking,
+    getT20TeamRanking,
+    getTeamsRanking,
+    getTestTeamRanking,
+    getODIPlayerBattingRanking,
+    getT20PlayerBattingRanking,
+    getTestPlayerBattingRanking,
+    getODIPlayerBowlingRanking,
+    getT20PlayerBowlingRanking,
+    getTestPlayerBowlingRanking,
+  } = require("../controller/ranking");
 
 
-module.exports = {
-  getODITeamRanking,
-  getTeamsRanking,
-  getTestTeamRanking,
-  getT20TeamRanking,
-};
+  router.get("/getTeamsRanking", async (req, res, next) => {
+    try {
+      const data = await getTeamsRanking();
+      res.json(data);
+    } catch {
+      (err) => {
+        throw new Error(err.message);
+      };
+    }
+  });
+  
+  router.get("/getOdiTeamRanking", async (req, res, next) => {
+    try {
+      const data = await getODITeamRanking();
+      res.json(data);
+    } catch {
+      (err) => {
+        throw new Error(err.message);
+      };
+    }
+  });
+  
+  router.get("/getT20TeamRanking", async (req, res, next) => {
+    try {
+      const data = await getT20TeamRanking();
+      res.json(data);
+    } catch {
+      (err) => {
+        throw new Error(err.message);
+      };
+    }
+  });
+  
+  router.get("/getTestTeamRanking", async (req, res, next) => {
+    try {
+      const data = await getTestTeamRanking();
+      res.json(data);
+    } catch {
+      (err) => {
+        throw new Error(err.message);
+      };
+    }
+  });
+  
+  router.get("/gett20PlayerBattingRanking", async (req, res ,next) => {
+    try{
+      const data = await getT20PlayerBattingRanking();
+      res.json(data);
+    }
+    catch{
+      err => {
+        throw new Error(err.message);
+      }
+    }
+  })
+  
+  router.get("/getOdiPlayerBattingRanking", async (req, res ,next) => {
+    try{
+      const data = await getODIPlayerBattingRanking();
+      res.json(data);
+    }
+    catch{
+      err => {
+        throw new Error(err.message);
+      }
+    }
+  })
+  
+  router.get("/getTestPlayerBattingRanking", async (req, res ,next) => {
+    try{
+      const data = await getTestPlayerBattingRanking();
+      res.json(data);
+    }
+    catch{
+      err => {
+        throw new Error(err.message);
+      }
+    }
+  })
+  
+  router.get("/gett20PlayerBowlingRanking", async (req, res ,next) => {
+    try{
+      const data = await getT20PlayerBowlingRanking();
+      res.json(data);
+    }
+    catch{
+      err => {
+        throw new Error(err.message);
+      }
+    }
+  })
+  
+  router.get("/getOdiPlayerBowlingRanking", async (req, res ,next) => {
+    try{
+      const data = await getODIPlayerBowlingRanking();
+      res.json(data);
+    }
+    catch{
+      err => {
+        throw new Error(err.message);
+      }
+    }
+  })
+  
+  router.get("/getTestPlayerBowlingRanking", async (req, res ,next) => {
+    try{
+      const data = await getTestPlayerBowlingRanking();
+      res.json(data);
+    }
+    catch{
+      err => {
+        throw new Error(err.message);
+      }
+    }
+  })
+
+  module.exports = router;
