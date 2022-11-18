1988062c80110fe0311aa469cd2887ffce2a0196
Muhammad Zaid <107930247+mdZaid165201b@users.noreply.github.com>
2022-11-19 01:37:51 +0500

    Update index.js

--- index.js
+++ index.js
@@ -2,15 +2,13 @@ const express = require("express");
 const dotenv = require("dotenv");
 const path = require("path");
 const bodyParser = require("body-parser");
-const { getLatestNews } = require("./Routes/news");
-const {
-  getODITeamRanking,
-  getT20TeamRanking,
-  getTeamsRanking,
-  getTestTeamRanking,
-} = require("./Routes/ranking");
+
+
+const rankingRoute = require("./Routes/ranking");
+const newsRoute = require("./Routes/news");
 
 const PORT = process.env.PORT || 3000;
+
 
 const app = express();
 dotenv.config();
@@ -21,60 +19,9 @@ app.get("/", (req, res, next) => {
   res.sendFile("public/index.html");
 });
 
-app.get("/getLatestNews", async (req, res, next) => {
-  try {
-    const data = await getLatestNews();
-    res.json(data);
-  } catch {
-    (err) => {
-      throw new Error(err.message);
-    };
-  }
-});
+app.use(rankingRoute);
+app.use(newsRoute);
 
-app.get("/getTeamsRanking", async (req, res, next) => {
-  try {
-    const data = await getTeamsRanking();
-    res.json(data);
-  } catch {
-    (err) => {
-      throw new Error(err.message);
-    };
-  }
-});
-
-app.get("/getOdiTeamRanking", async (req, res, next) => {
-  try {
-    const data = await getODITeamRanking();
-    res.json(data);
-  } catch {
-    (err) => {
-      throw new Error(err.message);
-    };
-  }
-});
-
-app.get("/getT20TeamRanking", async (req, res, next) => {
-  try {
-    const data = await getT20TeamRanking();
-    res.json(data);
-  } catch {
-    (err) => {
-      throw new Error(err.message);
-    };
-  }
-});
-
-app.get("/getTestTeamRanking", async (req, res, next) => {
-  try {
-    const data = await getTestTeamRanking();
-    res.json(data);
-  } catch {
-    (err) => {
-      throw new Error(err.message);
-    };
-  }
-});
 
 app.use((req, res, next) => {
   res.status(404).send({ message: "Not Found" });
@@ -82,4 +29,5 @@ app.use((req, res, next) => {
 
 app.listen(PORT, () => {
   console.log(`Server is listening on PORT ${PORT}`);
+  
 });
