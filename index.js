2ee9335f57570f012c5d5f42a8e3641e821176c7
Muhammad Zaid <107930247+mdZaid165201b@users.noreply.github.com>
2022-11-18 16:53:13 +0500

    Update index.js

--- index.js
+++ index.js
@@ -2,14 +2,20 @@ const express = require("express");
 const dotenv = require("dotenv");
 const path = require("path");
 const bodyParser = require("body-parser");
-const getLatestNews = require("./utility");
+const { getLatestNews } = require("./Routes/news");
+const {
+  getODITeamRanking,
+  getT20TeamRanking,
+  getTeamsRanking,
+  getTestTeamRanking,
+} = require("./Routes/ranking");
+
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 dotenv.config();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
-
 
 app.get("/", (req, res, next) => {
   res.sendFile("public/index.html");
@@ -18,6 +24,50 @@ app.get("/", (req, res, next) => {
 app.get("/getLatestNews", async (req, res, next) => {
   try {
     const data = await getLatestNews();
+    res.json(data);
+  } catch {
+    (err) => {
+      throw new Error(err.message);
+    };
+  }
+});
+
+app.get("/getTeamsRanking", async (req, res, next) => {
+  try {
+    const data = await getTeamsRanking();
+    res.json(data);
+  } catch {
+    (err) => {
+      throw new Error(err.message);
+    };
+  }
+});
+
+app.get("/getOdiTeamRanking", async (req, res, next) => {
+  try {
+    const data = await getODITeamRanking();
+    res.json(data);
+  } catch {
+    (err) => {
+      throw new Error(err.message);
+    };
+  }
+});
+
+app.get("/getT20TeamRanking", async (req, res, next) => {
+  try {
+    const data = await getT20TeamRanking();
+    res.json(data);
+  } catch {
+    (err) => {
+      throw new Error(err.message);
+    };
+  }
+});
+
+app.get("/getTestTeamRanking", async (req, res, next) => {
+  try {
+    const data = await getTestTeamRanking();
     res.json(data);
   } catch {
     (err) => {
